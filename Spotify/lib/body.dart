e92934f22607e8ad9907b1945746ed7828a7e742
LGRVision <lucasgabrielroch58@gmail.com>
2024-04-29 13:05:55 -0300

    commit

--- Spotify/lib/body.dart
+++ Spotify/lib/body.dart
@@ -1,257 +1,3 @@
-/*import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
-import '../data/artistas.dart';
-import '../data/musica.dart';
-import '../data/favoritos.dart';
-import '../data/playlist.dart';
-import '../detalhes/detalhes_da_musica.dart';
-import '../detalhes/detalhes_do_artista.dart';
-import '../detalhes/detalhes_do_album.dart';
-
-class Body extends StatefulWidget {
-  final List<Artista> artistas;
-  final List<Musica> musicas;
-  final List<Favorito> favoritos;
-  final List<Playlist> playlist;
-  final double listHeight;
-
-  const Body({
-    Key? key,
-    required this.artistas,
-    required this.musicas,
-    required this.favoritos,
-    required this.playlist,
-    required this.listHeight,
-  }) : super(key: key);
-
-  @override
-  _BodyState createState() => _BodyState();
-}
-
-class _BodyState extends State<Body> {
-  bool isDarkMode = false;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: isDarkMode ? Colors.black : Colors.white,
-      body: Column(
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Text(
-              "Bem-vindo de volta!",
-              style: TextStyle(
-                fontSize: 24,
-                color: isDarkMode ? Colors.white : Colors.black,
-              ),
-            ),
-          ),
-          buildBody(isDarkMode, widget, 350),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            isDarkMode = !isDarkMode;
-          });
-        },
-        child: const Icon(Icons.brightness_medium),
-      ),
-    );
-  }
-}
-
-Widget buildBody(isDarkMode, widget, listHeight) {
-  return Container(
-    color: isDarkMode ? Colors.black : Colors.white,
-    child: SingleChildScrollView(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0),
-            child: Center(
-              child: Text(
-                "Artistas",
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                  color: isDarkMode ? Colors.white : Colors.black,
-                ),
-              ),
-            ),
-          ),
-          const SizedBox(height: 8),
-          Container(
-            height: 140,
-            child: ListView.builder(
-              scrollDirection: Axis.horizontal,
-              itemCount: widget.artistas.length,
-              itemBuilder: (context, index) {
-                return Container(
-                  margin: const EdgeInsets.all(8.0),
-                  child: Column(
-                    children: <Widget>[
-                      InkWell(
-                        onTap: () {
-                          if (kDebugMode) {
-                            print(widget.artistas[index].nome + 'pressed');
-                          }
-                          Navigator.of(context).push(
-                            MaterialPageRoute(
-                              builder: (context) => DetalhesDoArtistaScreen(
-                                artista: widget.artistas[index],
-                              ),
-                            ),
-                          );
-                        },
-                        child: CircleAvatar(
-                          radius: 40,
-                          backgroundImage:
-                              AssetImage(widget.artistas[index].imagemUrl),
-                        ),
-                      ),
-                      Container(
-                        margin: const EdgeInsets.all(10.0),
-                        child: Text(
-                          widget.artistas[index].nome,
-                          style: TextStyle(
-                            color: isDarkMode ? Colors.white : Colors.black,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                );
-              },
-            ),
-          ),
-          const SizedBox(height: 16),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0),
-            child: Center(
-              child: Text(
-                "Músicas",
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                  color: isDarkMode ? Colors.white : Colors.black,
-                ),
-              ),
-            ),
-          ),
-          const SizedBox(height: 8),
-          Theme(
-            data: isDarkMode ? ThemeData.dark() : ThemeData.light(),
-            child: SizedBox(
-              height: listHeight,
-              child: ListView.builder(
-                itemCount: widget.musicas.length,
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    leading: CircleAvatar(
-                      backgroundImage:
-                          AssetImage(widget.musicas[index].capaUrl),
-                    ),
-                    title: Text(
-                      widget.musicas[index].titulo,
-                      style: TextStyle(
-                        color: isDarkMode ? Colors.white : Colors.black,
-                      ),
-                    ),
-                    subtitle: Text(
-                      widget.musicas[index].artista.nome,
-                      style: TextStyle(
-                        color: isDarkMode ? Colors.white : Colors.black,
-                      ),
-                    ),
-                    trailing: IconButton(
-                      icon: Icon(Icons.more_vert),
-                      onPressed: () {
-                        _mostrarOpcoes(context, widget.musicas[index]);
-                      },
-                    ),
-                    onTap: () {
-                      Navigator.of(context).push(
-                        MaterialPageRoute(
-                          builder: (context) => DetalhesDaMusicaScreen(
-                            musica: widget.musicas[index],
-                          ),
-                        ),
-                      );
-                    },
-                  );
-                },
-              ),
-            ),
-          ),
-        ],
-      ),
-    ),
-  );
-}
-
-_mostrarOpcoes(BuildContext context, Musica musica) {
-  showDialog(
-    context: context,
-    builder: (BuildContext context) {
-      return AlertDialog(
-        title: Text("Opções"),
-        content: SingleChildScrollView(
-          child: ListBody(
-            children: <Widget>[
-              ListTile(
-                leading: Icon(Icons.person),
-                title: Text("Detalhes do Artista"),
-                onTap: () {
-                  Navigator.of(context).pop();
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (context) => DetalhesDoArtistaScreen(
-                        artista: musica.artista,
-                      ),
-                    ),
-                  );
-                },
-              ),
-              ListTile(
-                leading: Icon(Icons.music_note),
-                title: Text("Detalhes da Música"),
-                onTap: () {
-                  Navigator.of(context).pop();
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (context) => DetalhesDaMusicaScreen(
-                        musica: musica,
-                      ),
-                    ),
-                  );
-                },
-              ),
-              ListTile(
-                leading: Icon(Icons.album),
-                title: Text("Álbum Correspondente"),
-                onTap: () {
-                  Navigator.of(context).pop();
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (context) => DetalhesDoAlbumScreen(
-                        album: musica.album,
-                      ),
-                    ),
-                  );
-                },
-              ),
-            ],
-          ),
-        ),
-      );
-    },
-  );
-}*/
-
-
 import 'package:Spotify/data/playlist.dart';
 import 'package:Spotify/detalhes/detalhes_da_musica.dart';
 import 'package:Spotify/detalhes/detalhes_do_artista.dart';
@@ -275,8 +21,9 @@ class Body extends StatefulWidget {
   _BodyState createState() => _BodyState();
 }
 
+bool isDarkMode = false;
+
 class _BodyState extends State<Body> {
-  bool isDarkMode = false;
 
   @override
   Widget build(BuildContext context) {
@@ -311,15 +58,20 @@ Widget buildBody(isDarkMode, widget, listHeight) {
     color: isDarkMode ? Colors.black : Colors.white, 
     child: SingleChildScrollView(
       child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 16.0),
-            child: Center(
-              child: Text(
-                "Artistas",
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: isDarkMode ? Colors.white : Colors.black), 
-              ),
+            child: Row(
+              // mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Icon(Icons.music_note_outlined, color: isDarkMode ? Colors.white : Colors.black),
+                Text(
+                  "Artistas",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: isDarkMode ? Colors.white : Colors.black), 
+                ),
+              ],
             ),
           ),
           const SizedBox(height: 8),
@@ -361,11 +113,16 @@ Widget buildBody(isDarkMode, widget, listHeight) {
           const SizedBox(height: 16),
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 16.0),
-            child: Center(
-              child: Text(
-                "Músicas",
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: isDarkMode ? Colors.white : Colors.black),
-              ),
+            child: Row(
+              // mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Icon(Icons.my_library_music_outlined, color: isDarkMode ? Colors.white : Colors.black),
+                Text(
+                  "Músicas",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: isDarkMode ? Colors.white : Colors.black), 
+                ),
+              ],
             ),
           ),
           const SizedBox(height: 8),
--- Spotify/lib/main.dart
+++ Spotify/lib/main.dart
@@ -1,10 +1,8 @@
-// ignore_for_file: unnecessary_import, prefer_const_constructors
-
-import 'package:Spotify/data/playlist.dart';
 import 'package:flutter/material.dart';
-import '../data/artistas.dart'; // Importe os dados dos artistas
-import '../data/musica.dart'; // Importe os dados das músicas
-import '../data/favoritos.dart'; // Importe os dados dos favoritos
+import 'package:Spotify/data/artistas.dart'; // Importe os dados dos artistas
+import 'package:Spotify/data/musica.dart'; // Importe os dados das músicas
+import 'package:Spotify/data/favoritos.dart'; // Importe os dados dos favoritos
+import 'package:Spotify/data/playlist.dart';
 import '../body.dart';
 import '../tabs/biblioteca_aba.dart';
 import '../tabs/favoritos_tab.dart';
@@ -13,66 +11,107 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({Key? key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  bool isOpened = false;
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    TextEditingController searchController = TextEditingController();
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: DefaultTabController(
         length: 3, // Número de abas
         child: Scaffold(
           appBar: AppBar(
+            automaticallyImplyLeading: false,
             backgroundColor: Colors.black,
-            title: const Text(
-              "Spotify",
-              style: TextStyle(
-                color: Colors.green,
-                fontWeight: FontWeight.bold,
-                fontSize: 30,
-              ),
-            ),
-            actions: [
-              AnimatedContainer(
-                duration: const Duration(milliseconds: 250),
-                width: isOpened ? 200 : 0,
-                height: 30,
-                color: Colors.white,
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(5.0),
-                  child: TextFormField(
-                    decoration: const InputDecoration(
-                      hintText: 'Pesquisar',
-                    ),
+            title: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Text(
+                  "Spotify",
+                  style: TextStyle(
+                    color: Colors.green,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30,
                   ),
                 ),
-              ),
-              IconButton(
-                icon: const Icon(Icons.search),
-                onPressed: () {
-                  setState(() {
-                    isOpened = !isOpened;
-                  });
-                },
-              ),
-              IconButton(
-                icon: const Icon(Icons.settings),
-                onPressed: () {},
-              ),
-            ],
-            bottom: const TabBar(
+                Expanded(child: Container()), // Adiciona espaço para que os elementos fiquem no lado direito
+                IconButton(
+                  icon: const Icon(Icons.search),
+                  onPressed: () {
+                    _showSearchSheet(context, searchController); // Abre a barra de pesquisa
+                  },
+                ),
+                CircleAvatar(
+                  radius: 20,
+                  backgroundImage: AssetImage('assets/usuario/Luska.jpg'), // Substitua pela imagem de perfil do usuário
+                ),
+                SizedBox(width: 10),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Nome do Usuário', // Substitua pelo nome do usuário
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 12,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      'Nível da Conta: Premium', // Substitua pelo nível da conta do usuário
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 8,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            bottom: TabBar(
               tabs: [
-                Tab(text: "Explorar"),
-                Tab(text: "Favoritos"),
-                Tab(text: "Biblioteca"),
+                Tab(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.explore, color: isDarkMode ? Colors.white : Colors.green[600]),
+                      SizedBox(width: 5),
+                      Text(
+                        "Explorar",
+                        style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold,),
+                      ),
+                    ],
+                  ),
+                ),
+                Tab(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.star, color: isDarkMode ? Colors.white : Colors.green[600]),
+                      SizedBox(width: 5),
+                      Text(
+                        "Favoritos",
+                        style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold,),
+                      ),
+                    ],
+                  ),
+                ),
+                Tab(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.dynamic_feed_sharp, color: isDarkMode ? Colors.white : Colors.green[600]),
+                      SizedBox(width: 5),
+                      Text(
+                        "Biblioteca",
+                        style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold,),
+                      ),
+                    ],
+                  ),
+                ),
               ],
             ),
           ),
@@ -80,11 +119,114 @@ class _MyAppState extends State<MyApp> {
             children: [
               Body(artistas: artistas, musicas: musicas, favoritos: favoritos, playlist: playlist, listHeight: 400),
               FavoritosTab(favoritos: favoritos),
-              BibliotecaTab(artistas: artistas, musicas: musicas, favoritos: favoritos, playlist: playlist)
+              BibliotecaTab(artistas: artistas, musicas: musicas, favoritos: favoritos, playlist: playlist),
             ],
+          ),
+          // Drawer
+          endDrawer: Drawer(
+            child: ListView(
+              padding: EdgeInsets.zero,
+              children: <Widget>[
+                DrawerHeader(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const [
+                      Row(
+                        children: [
+                          CircleAvatar(
+                            radius: 30,
+                            backgroundImage: AssetImage('assets/usuario/Luska.jpg'), // Substitua pela imagem de perfil do usuário
+                          ),
+                          SizedBox(width: 10),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'Luska Biel', // Substitua pelo nome do usuário
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Text(
+                                'Nível da Conta: Premium', // Substitua pelo nível da conta do usuário
+                                style: TextStyle(
+                                  fontSize: 10,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  decoration: BoxDecoration(
+                    color: Colors.green[600],
+                  ),
+                ),
+                ListTile(
+                  title: Text('Minha Conta'),
+                  onTap: () {
+                    // Adicione a lógica para a tela "Minha Conta" aqui
+                  },
+                ),
+                ListTile(
+                  title: Text('Sobre Nós'),
+                  onTap: () {
+                    // Adicione a lógica para a tela "Sobre Nós" aqui
+                  },
+                ),
+              ],
+            ),
           ),
         ),
       ),
+    );
+  }
+
+  void _showSearchSheet(BuildContext context, TextEditingController searchController) {
+    showModalBottomSheet(
+      context: context,
+      isScrollControlled: true,
+      builder: (BuildContext context) {
+        return SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+            child: Container(
+              padding: EdgeInsets.all(20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    'Pesquisar',
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 20),
+                  TextField(
+                    controller: searchController,
+                    decoration: InputDecoration(
+                      hintText: 'Digite sua pesquisa...',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () {
+                      // Adicione a lógica para pesquisar aqui
+                      String pesquisa = searchController.text;
+                      print('Pesquisa realizada: $pesquisa');
+                      Navigator.pop(context); // Fecha o bottom sheet após a pesquisa
+                    },
+                    child: Text('Pesquisar'),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
     );
   }
 }
